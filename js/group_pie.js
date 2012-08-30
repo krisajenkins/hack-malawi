14d48f3ca17d722d40f49d6d163d5afbba7a9549
Kris Jenkins <krisajenkins@gmail.com>
2012-08-30 10:14:25 +0100

    Latest merge from master branch.

--- js/group_pie.js
+++ js/group_pie.js
@@ -1,19 +1,15 @@
+/*jslint browser: true, nomen: true*/
+/*global d3: true, $: true, lookup: true*/
+"use strict";
+
 var width = 600;
 var height = 500;
-var minEdge = Math.min( width, height );
+var minEdge = Math.min(width, height);
 var colourScheme = d3.scale.category20();
 
 var arcGenerator = d3.svg.arc()
-	.outerRadius( minEdge * 0.46 )
-	.innerRadius( minEdge * 0.30 );
-
-function arcTween(data, index, current) {
-	var i = d3.interpolateObject( this._current, data );
-
-	return function(t) {
-		return arcGenerator(i(t));
-	};
-}
+	.outerRadius(minEdge * 0.46)
+	.innerRadius(minEdge * 0.30);
 
 // The closure is probably overkill. :-)
 var titles = {
@@ -24,35 +20,45 @@ var titles = {
 var next_key = (function () {
 	var items = Object.keys(titles);
 
-	return function() {
+	return function () {
 		var item = items.shift();
 		items.push(item);
 		return item;
 	};
 }());
 
-function calculate_list_item_top(d,i) { return ( 40 * i ) + "px"; }
+function calculate_list_item_top(d, i) { return (40 * i) + "px"; }
 function indexed_delay(d, i) { return i * 100; }
 
-$(document).ready(function() {
+$(document).ready(function () {
 	var svg = d3.select('#data').append("svg:svg")
 		.attr("width", width)
-		.attr("height", height);
+		.attr("height", height),
 
-	var pieGroup = svg.append("svg:g")
+		pieGroup = svg.append("svg:g")
 		.attr("id", "pies")
-		.attr("transform", "translate(" + ( width / 2 ) + "," + ( height / 2 ) + ")" );
+		.attr("transform", "translate(" + (width / 2) + "," + (height / 2) + ")");
 
-	d3.csv("group_pie.csv", function(response) {
-		var key = next_key();
+	d3.csv("group_pie.csv", function (response) {
+		var key = next_key(),
+			pieLayout = d3.layout.pie()
+				.value(lookup(key)),
 
-		var pieLayout = d3.layout.pie()
-			.value(lookup(key));
+			listItems = d3.select("#data").append("div")
+				.attr("id", "list")
+				.selectAll("div")
+				.data(response, lookup('id')),
 
-		var listItems = d3.select("#data").append("div")
-			.attr("id", "list")
-			.selectAll("div")
-			.data(response, lookup('id'));
+			pie = pieGroup.selectAll("path")
+				.data(pieLayout(response)),
+
+			pieTitle = d3.select("h3#title")
+				.text(titles[key]),
+
+			arcs = pie.enter().append("svg:path")
+				.attr("fill", function (d, i) { return colourScheme(i); })
+				.attr("d", arcGenerator)
+				.each(function (d) { this._current = d; });
 
 		listItems.enter().append("div")
 				.attr("class", "listItem")
@@ -63,23 +69,12 @@ $(document).ready(function() {
 
 		listItems.append("div")
 			.attr("class", "listItemBox")
-			.style("background-color", function(d,i) { return colourScheme(i); });
+			.style("background-color", function (d, i) { return colourScheme(i); });
 
 		listItems.append("div")
 			.text(lookup('donor'));
 
-		var pie = pieGroup.selectAll("path")
-			.data(pieLayout(response));
-
-		var pieTitle = d3.select("h3#title")
-			.text(titles[key]);
-
-		var arcs = pie.enter().append("svg:path")
-			.attr("fill", function(d,i) { return colourScheme(i); })
-			.attr("d", arcGenerator)
-			.each(function(d) { this._current = d; });
-
-		d3.select(window).on("click", function() {
+		d3.select(window).on("click", function () {
 			var key = next_key();
 
 			pieLayout.value(lookup(key));
@@ -87,15 +82,21 @@ $(document).ready(function() {
 
 			arcs.transition()
 				.delay(indexed_delay)
-				.attrTween("d", arcTween)
-				.each("end", function(d) { this._current = d; });
+				.attrTween("d", function (data, index, current) {
+					var i = d3.interpolateObject(this._current, data);
+
+					return function (t) {
+						return arcGenerator(i(t));
+					};
+				})
+				.each("end", function (d) { this._current = d; });
 
 			listItems.data(response.sort_by_key(key), lookup('id'))
 				.transition()
 					.delay(indexed_delay)
 					.style("top", calculate_list_item_top);
 
-			pieTitle.text( titles[key] );
+			pieTitle.text(titles[key]);
 		});
 	});
 });
--- js/projection.js
+++ js/projection.js
@@ -1,10 +1,14 @@
+/*jslint browser: true*/
+/*global d3: true, $: true, identity: true, lookup: true*/
+"use strict";
+
 var projection = d3.geo.azimuthal()
 	.mode("equidistant")
 	.origin([38.2, -12.5])
 	.scale(4000);
 
 function project_object(object) {
-	var projected_point = projection( [object.longitude, object.latitude] );
+	var projected_point = projection([object.longitude, object.latitude]);
 
 	object.projected_longitude = projected_point[0];
 	object.projected_latitude  = projected_point[1];
@@ -15,39 +19,78 @@ function project_object(object) {
 var data_to_path = d3.geo.path()
 	.projection(projection);
 
-$(document).ready(function() {
+$(document).ready(function () {
 	var svg = d3.select("#data")
-		.insert("svg:svg","#filters")
+		.insert("svg:svg", "#filters")
 		.attr("width", 400)
-		.attr("height", 600);
+		.attr("height", 600),
 
-	var mapGroup = svg.append("svg:g")
-		.attr("id", "map");
+		mapGroup = svg.append("svg:g")
+		.attr("id", "map"),
 
-	var circleGroup = svg.append("svg:g")
+		circleGroup = svg.append("svg:g")
 		.attr("id", "circles");
 
-	d3.json("json/malawi.geojson", function(collection) {
+	d3.json("json/malawi.geojson", function (collection) {
 		var mapPaths = mapGroup.selectAll("path")
 			.data([collection]);
 
 		mapPaths.enter()
 			.append("svg:path")
-			.attr("d", function(data) { return data_to_path( data ); });
+			.attr("d", function (data) { return data_to_path(data); });
 
 		mapPaths.exit().remove();
 	});
 
-	d3.csv("Malawi_Digested.csv", function(response) {
+	d3.csv("Malawi_Digested.csv", function (response) {
 		// Project the points for each project.
-		var projects = response.map(project_object);
+		var projects = response.map(project_object),
+			plot_projects = function (projects_to_plot) {
+				// Add the circles.
+				var circles = circleGroup.selectAll("circle")
+					.data(projects_to_plot, lookup('id'));
+
+				circles.enter().append("svg:circle")
+					.attr("opacity",  0.0)
+					.attr("r",        0.0)
+					.attr("cx",       -1000)
+					.attr("cy",       function (project) { return project.projected_latitude; })
+					.attr("stroke-width", "0px")
+					.transition()
+						.duration(250)
+						.attr("opacity",  function (project) { return 1.0 / project.precision; })
+						.attr("cx",       function (project) { return project.projected_longitude; })
+						.attr("r",        function (project) { return project.precision * 5.0; });
+
+				circles.exit()
+					.transition()
+						.duration(250)
+						.attr("opacity", 0.0)
+						.attr("cx", 1000.0)
+						.attr("r", 0.0)
+					.remove();
+
+				circles.on("mouseover", function (project) {
+					var detail = $("#detail");
+					detail.fadeIn();
+					detail.find("#amp_sector").text(project.amp_sector);
+					detail.find("#type_of_assistance").text(project.type_of_assistance);
+					detail.find("#status").text(project.status);
+					detail.find("#funding").text("$" + Number(project.funding).toMoney(0));
+					detail.find("#donor").text(project.donor);
+
+					$("circle").attr("stroke-width", "0px");
+					$("circle").attr("opacity", (1.0 / project.precision));
+
+					$(this).attr("stroke-width", "5px");
+					$(this).attr("opacity",  1);
+				});
+			};
 
 		function generate_filters(filter_key) {
-			var group_names = projects.map(function(object) { return object[filter_key]; }).sort_unique();
-
-			// Add the filter list of groups.
-			var filterDivs = d3.select("#filters").selectAll("div")
-				.data(group_names, identity);
+			var group_names = projects.map(function (object) { return object[filter_key]; }).sort_unique(),
+				filterDivs = d3.select("#filters").selectAll("div")
+					.data(group_names, identity);
 
 			filterDivs.enter().append("div")
 				.attr("class", "filter")
@@ -55,13 +98,13 @@ $(document).ready(function() {
 
 			filterDivs.exit().remove();
 
-			filterDivs.on("click", function(group_name) {
+			filterDivs.on("click", function (group_name) {
 				// Mark me as selected.
 				$(".filter").removeClass("selected");
 				$(this).addClass("selected");
 
 				// Replot the relevant projects.
-				var filtered_projects = projects.filter(function(object) { return object[filter_key] === group_name; } );
+				var filtered_projects = projects.filter(function (object) { return object[filter_key] === group_name; });
 				plot_projects(filtered_projects);
 
 				$("#detail").fadeOut();
@@ -71,51 +114,6 @@ $(document).ready(function() {
 		}
 
 		generate_filters('amp_sector');
-
-		var plot_projects = function(projects_to_plot) {
-			// Add the circles.
-			var circles = circleGroup.selectAll("circle")
-				.data(projects_to_plot, lookup('id'))
-
-			circles.enter().append("svg:circle")
-				.attr("opacity",  0.0)
-				.attr("r",        0.0)
-				.attr("cx",       -1000)
-				.attr("cy",       function(project) { return project.projected_latitude; })
-				.attr("stroke-width", "0px")
-				.transition()
-					.duration( 250 )
-					.attr("opacity",  function(project) { return 1.0 / project.precision; })
-					.attr("cx",       function(project) { return project.projected_longitude; })
-					.attr("r",        function(project) { return project.precision * 5.0; });
-
-			circles.exit()
-				.transition()
-					.duration( 250 )
-					.attr("opacity", 0.0 )
-					.attr("cx", 1000.0 )
-					.attr("r", 0.0 )
-				.remove();
-
-			circles.on("mouseover", function(project) {
-				var detail = $("#detail");
-				detail.fadeIn();
-				detail.find("#amp_sector").text( project.amp_sector );
-				detail.find("#type_of_assistance").text( project.type_of_assistance );
-				detail.find("#status").text( project.status );
-				detail.find("#funding").text( "$" + Number( project.funding ).toMoney(0) );
-				detail.find("#donor").text( project.donor );
-
-				$("circle").attr("stroke-width", "0px");
-				$("circle").attr("opacity",  ( 1.0 / project.precision ));
-
-				$(this).attr("stroke-width", "5px");
-				$(this).attr("opacity",  1 );
-			});
-
-			circles.on("mouseout", function(project) {
-			});
-		};
 
 		// For convenience, click one to kick us off.
 		$(".filter")[0].click();
